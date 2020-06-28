032d65b9825903a26ba7fe1846918c9ce55d6a98
TAKAHASHI,Toru <torutk@gmail.com>
2020-06-28 21:14:15 +0900

    Modify module name based on the package name

--- src/module-info.java
+++ src/module-info.java
@@ -2,7 +2,7 @@
  *  Copyright © 2020 TAKAHASHI,Toru
  */
 
-module GadgetSupport {
+module com.torutk.gadget.support {
     requires javafx.controls;
     requires java.prefs;
     exports com.torutk.gadget.support;
